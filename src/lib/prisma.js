65478ad024007b80162a839c7bfca1a0754dd707
hadiasta <hijrihadi@gmail.com>
2025-07-23 10:26:44 +0700

    trigger redeploy #4 debuging prisma problem

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,30 +1,43 @@
+// lib/prisma.js
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient({
-  log: [
-    {
-      emit: "event",
-      level: "query",
-    },
-    {
-      emit: "stdout",
-      level: "error",
-    },
-    {
-      emit: "stdout",
-      level: "info",
-    },
-    {
-      emit: "stdout",
-      level: "warn",
-    },
-  ],
-});
+const globalForPrisma = globalThis;
 
-prisma.$on("query", (e) => {
-  console.log("Query: " + e.query);
-  console.log("Params: " + e.params);
-  console.log("Duration: " + e.duration + "ms");
-});
+let prisma;
+
+if (!globalForPrisma.prisma) {
+  prisma = new PrismaClient({
+    log: [
+      {
+        emit: "event",
+        level: "query",
+      },
+      {
+        emit: "stdout",
+        level: "error",
+      },
+      {
+        emit: "stdout",
+        level: "info",
+      },
+      {
+        emit: "stdout",
+        level: "warn",
+      },
+    ],
+  });
+
+  prisma.$on("query", (e) => {
+    console.log("Query:", e.query);
+    console.log("Params:", e.params);
+    console.log("Duration:", e.duration + "ms");
+  });
+
+  if (process.env.NODE_ENV !== "production") {
+    globalForPrisma.prisma = prisma;
+  }
+} else {
+  prisma = globalForPrisma.prisma;
+}
 
 export default prisma;
